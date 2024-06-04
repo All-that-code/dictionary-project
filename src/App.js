4b012c92d1bd3b8a92feb535e19cc7cb1c85a275
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-06-04 21:57:20 +0200

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import words from "./words.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,10 @@ function App() {
       <header className="App-header">
         <img src={words} className="words-logo img-fluid" alt="tittle" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Amaia Arbizu</footer>
     </div>
   );
 }
