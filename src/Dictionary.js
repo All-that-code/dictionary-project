ab356b6f847781ea340c73ee66738ef6d779ca07
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-06-04 22:25:04 +0200

    imported Axios and added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl =
+      "https://api.shecodes.io/dictionary/v1/define?word=sunset&key=8o03bb70ba39844fdc4a5a5t25cc70b6";
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
