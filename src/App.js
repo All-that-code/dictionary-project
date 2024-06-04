fda67f7c936970f9c8b572148ec802be2d41f360
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-06-04 20:15:32 +0200

    Initial command

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import words from "./words.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={words} className="words-logo img-fluid" alt="tittle" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
