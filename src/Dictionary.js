728ee557d2e63efa3b28c57ef23ddba91e94c6cd
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-06-05 21:27:18 +0200

    Rendered the meanings and results components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiKey = "8o03bb70ba39844fdc4a5a5t25cc70b6";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
@@ -32,6 +33,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
