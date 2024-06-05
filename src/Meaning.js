cd72597096ec01ff5c7a6ac59f66731c0ee6d12e
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-06-05 22:23:05 +0200

    Added some very simple styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
-    <div className="Meaning">
+    <div className="WordMeaning mt-5 mb-5">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <div className="Definition text-capitalize mt-3">
+        <p>{props.meaning.definition}</p>
+      </div>
+      <div className="Example text-capitalize">
+        <p>{props.meaning.example}</p>
+      </div>
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -5,10 +5,12 @@ import Meaning from "./Meaning";
 export default function Results(props) {
   if (props.results) {
     return (
-      <div className="Results">
-        <h2>{props.results.word}</h2>
-        <h3>{props.results.phonetic}</h3>
-        {props.results.meanings.map(function (meaning, index) {
+      <div className="Results text-left ms-5 mt-5">
+        <div className="text-capitalize">
+          <h2>{props.results.word}</h2>
+        </div>
+        <h3>/{props.results.phonetic}/</h3>
+        {props.results.meanings.slice(0, 3).map(function (meaning, index) {
           return (
             <div key={index}>
               <Meaning meaning={meaning} />
